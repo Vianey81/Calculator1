328792d578e8deb3cde0f573b43daaa66cb2dd4f
Hackbright Student <info@hackbrightacademy.com>
2016-01-06 11:32:28 -0800

    Add square

--- arithmetic.py
+++ arithmetic.py
@@ -11,8 +11,8 @@ def multiply(num1, num2):
 def divide(num1, num2):
     return num1 / num2
 
-# def square(num1):
-#     pass
+def square(num1):
+    return num1 * num1
 
 # def cube(num1):
 #     pass
