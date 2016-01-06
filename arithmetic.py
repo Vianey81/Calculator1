0e0a8d0c92b00e99d5e4a4d68a9c52c362add193
Hackbright Student <info@hackbrightacademy.com>
2016-01-06 11:31:47 -0800

    add Division

--- arithmetic.py
+++ arithmetic.py
@@ -8,8 +8,8 @@ def subtract(num1, num2):
 def multiply(num1, num2):
     return num1 * num2
 
-# def divide(num1, num2):
-#     pass
+def divide(num1, num2):
+    return num1 / num2
 
 # def square(num1):
 #     pass
