ff0f94f8bdb52d3cc36232734e2de947d7cbfe49
Hackbright Student <info@hackbrightacademy.com>
2016-01-06 11:33:43 -0800

    Add Cube

--- arithmetic.py
+++ arithmetic.py
@@ -14,8 +14,8 @@ def divide(num1, num2):
 def square(num1):
     return num1 * num1
 
-# def cube(num1):
-#     pass
+def cube(num1):
+    return num1 * num1 * num1
 
 # def power(num1, num2):
 #     pass
